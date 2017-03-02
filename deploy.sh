615c859b8cad27267b766a009985af5a3d16573b
Saivickna Raveendran <saivickna.r@gmail.com>
2017-03-01 16:37:59 -0800

    (feat) Add files for CircleCI deployment

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+echo "stopping running application"
+ssh $DEPLOY_USER@$DEPLOY_HOST 'docker stop ngage-stats'
+ssh $DEPLOY_USER@$DEPLOY_HOST 'docker rm ngage-stats'
+
+echo "pulling latest version of the code"
+ssh $DEPLOY_USER@$DEPLOY_HOST 'docker pull nanongage/ngage-stats:latest'
+
+echo "starting the new version"
+ssh $DEPLOY_USER@$DEPLOY_HOST 'docker run -d --restart=always -e DBIP="'$DBIP'" -e DBDB="'$DBDB'" -e DBPW="'$DBPW'" --name ngage-stats -p 4555:4555 nanongage/ngage-stats:latest'
+
+echo "success!"
+
+exit 0
